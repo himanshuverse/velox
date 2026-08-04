bd8f40480f4ff8e9f082bc9115049d6ab93363bb
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-04 19:08:26 +0530

    feat: integrate Prisma for database management with PostgreSQL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button"
+import prisma from "@/lib/db"
 
 
-const page = () => {
+const   page =async () => {
   return (
     <div className="text-red-400 min-h-screen min-w-screen flex items-center justify-center">
       <Button >
