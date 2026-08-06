8ff2cac68929d60f6be7e749d087b3d7c055da6a
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-06 23:57:14 +0530

    feat : add Client component with TRPC data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { Button } from "@/components/ui/button"
-import prisma from "@/lib/db"
-
-
+import { caller, getQueryClient } from "@/trpc/server"
+import { trpc } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import { Suspense } from "react"
+import { Client } from "./client"
 const   page =async () => {
+
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
   return (
-    <div className="text-red-400 min-h-screen min-w-screen flex items-center justify-center">
-      <Button >
-        click me 
-      </Button>
+    <div className="text-green-400 min-h-screen min-w-screen flex items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>loading....</p>}>
+          <Client/>
+        </Suspense>
+      </HydrationBoundary>
     </div>
   )
 }
