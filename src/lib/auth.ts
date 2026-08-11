b22e105aeb2581ddfd378a51b13dc9e6944635b7
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-11 23:42:40 +0530

    feat(prisma): enhance User, Post, Session, Account, and Verification models with additional fields and constraints

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
 import { betterAuth } from "better-auth";
 import {prismaAdapter} from "better-auth/adapters/prisma"
-import {prisma} from "@/lib/db"
+import prisma from "./db"
 
 export const auth = betterAuth({
-    database:prismaAdapter(prisma ,{provider:"postgresql"})
+    database:prismaAdapter(prisma ,{provider:"postgresql"}),
+    emailAndPassword: { 
+    enabled: true, 
+  }, 
 })
