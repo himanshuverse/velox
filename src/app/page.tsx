47a6abf153c50353b09c7ce800b36e74b591fb66
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-18 01:31:27 +0530

    feat(auth): implement requireAuth and requireUnauth functions for session management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-'use client'
+import { requireAuth } from "@/lib/auth-utils"
 
-import { Button } from "@/components/ui/button"
-import { authClient } from "@/lib/auth-client"
+const page = async  () => {
 
-const page =  () => {
-  const { data } =authClient.useSession()
+  await requireAuth()
+
   return (
     <div className="text-green-400 min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(data)}
-      <Button onClick={()=>authClient.signOut()}>
-        Logout
-      </Button>
+      server protected component
     </div>
   )
 }
