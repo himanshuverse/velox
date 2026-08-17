87fab2d273f5124547ebd524cd405abb7f088d04
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-18 01:50:30 +0530

    feat(auth): implement protected procedures trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { requireAuth } from "@/lib/auth-utils"
+import { caller } from "@/trpc/server"
 
 const page = async  () => {
 
   await requireAuth()
 
+  const data=await caller.getUsers()
+
   return (
     <div className="text-green-400 min-h-screen min-w-screen flex items-center justify-center">
       server protected component
+      {JSON.stringify(data , null , 2)}
     </div>
   )
 }
