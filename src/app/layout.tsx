cda0d97162f76881e67a66621346270d4189ee42
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-19 20:38:17 +0530

    feat(theme): wrap children with ThemeProvider in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/themeprovider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <TRPCReactProvider>
+          <ThemeProvider>
           {children}
           <Toaster/>
+          </ThemeProvider>
         </TRPCReactProvider>
       </body>
     </html>
