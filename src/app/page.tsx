853bfbc868c72eb0d0a8e5c51f54279eaae54254
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-28 16:46:08 +0530

    feat(workflow): implement create workflow functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
+'use client'
+
+
+import { Button } from "@/components/ui/button"
 import { requireAuth } from "@/lib/auth-utils"
+import { useTRPC } from "@/trpc/client"
 import { caller } from "@/trpc/server"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
-const page = async  () => {
+const page =   () => {
 
-  await requireAuth()
+  const trpc=useTRPC()
+  const queryClient =useQueryClient()
+  const {data} =useQuery(trpc.getWorkflows.queryOptions())
+  // await requireAuth()
 
-  const data=await caller.getUsers()
+  const create= useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess:()=>{
+      queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+    }
+  }))
 
   return (
-    <div className="text-green-400 min-h-screen min-w-screen flex items-center justify-center">
+    <div className="text-green-400 min-h-screen min-w-screen flex items-center justify-center ">
       server protected component
       {JSON.stringify(data , null , 2)}
+
+      <Button className="bg-white"  disabled={create.isPending} onClick={()=>create.mutate()}>
+        Create Workflow
+      </Button>
     </div>
   )
 }
