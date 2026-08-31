d3c8011b282d2e2ae1b5f8e2954ec5ef23ed117e
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-08-31 20:30:20 +0530

    feat: add Workflow and WorkflowRun models to schema

---
+++ prisma/migrations/20260830201522_workflow_and_workflow_run_add/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Workflow` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Workflow" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "Workflow_userId_idx" ON "Workflow"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Workflow" ADD CONSTRAINT "Workflow_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
