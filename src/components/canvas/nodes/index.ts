62cb9216906f93f7d6d7170fe56dcda217acb377
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-09-04 15:11:40 +0530

    feat: add  nodeType mapping index file

---
+++ src/components/canvas/nodes/index.ts
@@ -0,0 +1,11 @@
+import { TriggerNode } from "./trigger-node";
+import { ActionNode } from "./action-node";
+import { ConditionNode } from "./condition-node";
+import { AiNode } from "./ai-node";
+
+export const nodeTypes = {
+  trigger: TriggerNode,
+  action: ActionNode,
+  condition: ConditionNode,
+  ai: AiNode,
+};
